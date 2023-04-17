57070b18254940c9d84146473db2e083584afd6f
Liliia Baieva <555blv@gmail.com>
2023-04-18 00:12:04 +0300

    add start program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("=================");
+    System.out.println(">> TRAVEL COST <<");
+    System.out.println("=================");
+    System.out.println("Программа для расчёта стоимтости поездки.");
+    GoTravel.goTravel();
+  }
+}
