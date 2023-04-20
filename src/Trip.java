7dcb17c628892b74125f0327416f07fa651d191e
Liliia Baieva <555blv@gmail.com>
2023-04-21 01:17:44 +0300

    delete lookTrip(), deleteTrip(), printAllTrips(), parseALLTrip,

--- src/Trip.java
+++ src/Trip.java
@@ -3,22 +3,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class Trips {
+public class Trip {
   public final static char SEP = '/';
-
-  //TODO  подменю чтоб посмотреть поездки
-  public static void lookTrip(){
-
-    //     - Вывести все поездки {номер, название стоимость общая, и за одного человека}
-    //    - Посмотреть одну поездку:
-    //        Вывести список поездок на экран
-    //    и выбрать, которая интересует
-    //    - в главное меню
-  }
 
   // TODO
   public static void editTrip(){
@@ -28,55 +16,70 @@ public class Trips {
     //  4. через свитч-кейс вызывает соответсвующую статье расходов функцию
   }
 
-  //TODO удаление записи
-  public static void deleteTrip(){
+  // TODO
+  public static void printTrip() throws IOException {
+    String name = getTripName();
+    for (int i = 0; i < name.length()+4; ++i){
+      System.out.print("*");
+    }
+    System.out.println("\n| " + name + " |");
+    for (int i = 0; i < name.length()+4; ++i){
+      System.out.print("*");
+    }
+    System.out.println();
+
+    List<Expense> tripList = parseTripList();
+    for( int i = 0; i < tripList.size(); ++i){
+      Expense expense = tripList.get(i);
+      if(i == tripList.size() - 1){
+        System.out.println("=============================================================");
+        System.out.println(expense);
+      } else {
+        System.out.println("_____________________________________________________________");
+        System.out.printf("[ %d ] %s \n", i,  expense);
+      }
+    }
+
+    System.out.println();
+
+    GoTravel.runMenu();
   }
 
-  //TODO здесь будем сравнивать поездки
-  public static void compareTrip(){
-    // 1. выводить на экран printAllTrip()
-    // 2. спрашивать какие поездки мы хотим сравнить и по какому показателю или всю
-    //     список статей расходов с номерами
-    // printComparableTrip(trip1, trip2, parameter)
-  }
-
-  // TODO
-  private static void printAllTrips(){}
-
-  // TODO
-  private static void printTrip(){}
-
-  private static Map<String, List> parseALLTrip() throws IOException {
+  // TODO читаем имя поездки
+  private static String getTripName() throws IOException{
     File tripsFile = new File("res/Trips.txt");
     if (!tripsFile.exists()) {
       System.out.println("Файл не найден. Или Вы не создали ещё не одной поездки.");
       GoTravel.runMenu();
     }
-
     BufferedReader br = new BufferedReader(new FileReader("res/Trips.txt"));
-    Map<String, List> allTrips = new HashMap<>();
-    for(String line = br.readLine(); line != null; line = br.readLine()){
-      int firstSep = line.indexOf(SEP);
-      String nameOfTrip = line.substring(0, firstSep);
-      String lineForList = line.substring(firstSep + 1);
-      List<Expense> expenses = parseTripList(lineForList);
 
-       allTrips.put(nameOfTrip, expenses);
-    }
-    return allTrips;
+    String line = br.readLine();
+    int sep = line.indexOf(SEP);
+    String name = line.substring(0, sep);
+    return name;
   }
 
-  private static List<Expense>  parseTripList(String lineForList){
-    List<Expense> expenses = new ArrayList<>();
+  public static List<Expense> parseTripList() throws IOException {
+    File tripsFile = new File("res/Trips.txt");
+    if (!tripsFile.exists()) {
+      System.out.println("Файл не найден. Или Вы не создали ещё не одной поездки.");
+      GoTravel.runMenu();
+    }
+    BufferedReader br = new BufferedReader(new FileReader("res/Trips.txt"));
 
-    for(int i = 0; lineForList.length() > 0; ++i){
+    String line = br.readLine();
+      // обрезаем строку минус имя
+    String lineForList = line.substring(getTripName().length() + 1);
+    List<Expense> expenses = new ArrayList<>();
+    while (lineForList.length() != 0){
       int lastSep = lineForList.indexOf(SEP); // находим разделитель
       String lineForeExp = lineForList.substring(0, lastSep); //вырезаем кусок, кот отправим на конструктор
       Expense expense = parseExpenses(lineForeExp);  // преобразуем кусок строки в констуктор
       expenses.add(expense); // добавляем конструктор
       lineForList = lineForList.substring(lastSep + 1); // вырезаем обработаный кусок сначала строки
     }
-
+    br.close();
     return expenses;
   }
 
